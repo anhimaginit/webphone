ec42815e89cc6801181e8aaee0de435aa5ef43d3
anh <anh@at1ts.com>
2022-04-11 11:54:49 +0700

    acl

--- public/acl.php
+++ public/acl.php
@@ -92,27 +92,11 @@ License: You must have a valid license purchased only from wrapbootstrap.com (li
 
                                 <!--tab-->
                                 <div class="panel-content m-t20">
-                                    <ul class="nav nav-tabs" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" data-toggle="tab" href="#tab_borders_icons-1" role="tab"> Home</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" data-toggle="tab" href="#tab_borders_icons-2" role="tab"> Profile</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" data-toggle="tab" href="#tab_borders_icons-3" role="tab"> Time</a>
-                                        </li>
+                                    <ul class="nav nav-tabs" id="ul-acl" role="tablist">
+
                                     </ul>
-                                    <div class="tab-content border border-top-0 p-3">
-                                        <div class="tab-pane fade show active" id="tab_borders_icons-1" role="tabpanel">
-                                            Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone.
-                                        </div>
-                                        <div class="tab-pane fade" id="tab_borders_icons-2" role="tabpanel">
-                                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic.
-                                        </div>
-                                        <div class="tab-pane fade" id="tab_borders_icons-3" role="tabpanel">
-                                            Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.
-                                        </div>
+                                    <div class="tab-content border border-top-0 p-3" id="body-acl">
+
                                     </div>
                                 </div>
                                 <!--endtab-->
--- public/js/group/acl.js
+++ public/js/group/acl.js
@@ -18,6 +18,7 @@ acl.prototype = {
         })
 
         $("#acl-view #g_id").change(function(){
+
             acl.prototype.getACL_gID($(this).val())
         })
 
@@ -56,6 +57,9 @@ acl.prototype = {
     },
 
     getACL_gID:function(g_id){
+        $('#acl-view #ul-acl').html("")
+        $('#acl-view #body-acl').html("")
+
         var _link =link._groups;
         var _data ={auth:_auth, limit:1,offset:0,g_id:g_id}
 
@@ -73,14 +77,8 @@ acl.prototype = {
                 if(res.response.row_cnt >0){
                     var data = res.response.results[0].acl;
                    // console.log(data.acl);
-                    var li ="";
-                    var div ="";
                     for (let key in data) {
-                        console.log(key+"=");
-                        //console.log(data[key]);
-                        for(let k_field in data[key]){
-                            console.log(k_field+"="+data[key][k_field]["add"]+",");
-                        }
+                        acl.prototype.processACLFields(key,data);
                     }
                 }
                 //
@@ -93,31 +91,174 @@ acl.prototype = {
         var div ='';
         var tr="";
         var tbody="";
+        var tab='';
+        var active ="";
         switch(key){
             case "Permission":
+                active = "active";
                 for(let k_field in obj){
-                    tr +='<tr class="tr-permission">' +
-                        '<td class="field"></td>'+
-                        '<td class="field"></td>'
+                    var view = obj[k_field]["view"];
+                    var add = obj[k_field]["add"];
+                    var edit = obj[k_field]["edit"];
+
+                    tr +='<tr class="tr_acl">' +
+                        '<td >'+k_field+'<input class="key" type="hidden" value="'+k_field+'"></td>'+
+                        acl.prototype.td_acl(view,add,edit)+
+                        '</tr>';
+                }
+
+                break;
+            case "Assigned_Integration":
+                for(let k_field in obj){
+                    var k_field_t = k_field.split("_")
+                    var key_name = k_field;
+                    if(k_field_t[0] =="i"){
+                        key_name = "Integration"
+                    }else if(k_field_t[0] =="u"){
+                        key_name = "User"
+                    }else{
+                        key_name = "Assigned Integration "+ k_field_t[1]
+                    }
+
+                    var view = obj[k_field]["view"];
+                    var add = obj[k_field]["add"];
+                    var edit = obj[k_field]["edit"];
+
+                    tr +='<tr class="tr_acl">' +
+                        '<td >'+key_name+'<input class="key" type="hidden" value="'+k_field+'"></td>'+
+                        acl.prototype.td_acl(view,add,edit)+
                         '</tr>';
                 }
                 break;
-            case "Assigned_Integration":
-                break;
             case "Branch":
+                for(let k_field in obj){
+                    var k_field_t = k_field.split("_")
+                    var key_name = k_field;
+                    if(k_field_t[0] =="c"){
+                        key_name = "Company"
+                    }else{
+                        key_name = "Branch "+ k_field_t[1]
+                    }
+
+                    var view = obj[k_field]["view"];
+                    var add = obj[k_field]["add"];
+                    var edit = obj[k_field]["edit"];
+
+                    tr +='<tr class="tr_acl">' +
+                        '<td >'+key_name+'<input class="key" type="hidden" value="'+k_field+'"></td>'+
+                        acl.prototype.td_acl(view,add,edit)+
+                        '</tr>';
+                }
                 break;
             case "Company":
+                for(let k_field in obj){
+                    var k_field_t = k_field.split("_")
+                    //var key_name = k_field;
+
+                    var key_name = "Company "+ k_field_t[1]
+
+                    var view = obj[k_field]["view"];
+                    var add = obj[k_field]["add"];
+                    var edit = obj[k_field]["edit"];
+
+                    tr +='<tr class="tr_acl">' +
+                        '<td >'+key_name+'<input class="key" type="hidden" value="'+k_field+'"></td>'+
+                        acl.prototype.td_acl(view,add,edit)+
+                        '</tr>';
+                }
                 break;
             case "Integration":
+                for(let k_field in obj){
+                    var k_field_t = k_field.split("_")
+                    var key_name = k_field;
+                    if(k_field_t[0] =="c"){
+                        key_name = "Company"
+                    }else{
+                        key_name = "Integration "+ k_field_t[1]
+                    }
+
+                    var view = obj[k_field]["view"];
+                    var add = obj[k_field]["add"];
+                    var edit = obj[k_field]["edit"];
+
+                    tr +='<tr class="tr_acl">' +
+                        '<td >'+key_name+'<input class="key" type="hidden" value="'+k_field+'"></td>'+
+                        acl.prototype.td_acl(view,add,edit)+
+                        '</tr>';
+                }
                 break;
             case "User":
+
+                for(let k_field in obj){
+                    var k_field_t = k_field.split("_")
+                    var key_name = k_field;
+                    if(k_field_t[0] =="b"){
+                        key_name = "Branch"
+                    }else{
+                        key_name = "User "+ k_field_t[1]
+                    }
+
+                    var view = obj[k_field]["view"];
+                    var add = obj[k_field]["add"];
+                    var edit = obj[k_field]["edit"];
+
+                    tr +='<tr class="tr_acl">' +
+                        '<td >'+key_name+'<input class="key" type="hidden" value="'+k_field+'"></td>'+
+                        acl.prototype.td_acl(view,add,edit)+
+                        '</tr>';
+                }
                 break;
         }
 
-        li +='<li class="nav-item">'+
-            '<a class="nav-link active" data-toggle="tab" href="#'+key+'" role="tab">'+key+'</a>'+
+        li +='<li class="nav-item ">'+
+            '<a class="nav-link '+active+'" data-toggle="tab" href="#'+key+'" role="tab">'+key+'</a>'+
             '</li>'
 
+        tab ='<div class="tab-pane fade show acl '+active+'" id="'+key+'" role="tabpanel">' +
+                '<div class="table-responsive-lg col-12">' +
+                    '<table class="table table-bordered m-0 t-normal-l table-acl">' +
+                        '<thead>'+
+                            '<tr>'+
+                                '<th style="width: 150px">Filed Name</th>'+
+                                '<th>Permission</th>'+
+                            '</tr>'+
+                        '</thead>'+
+                        '<tbody>'+
+                            tr
+                        '</tbody>' +
+                    '</table>' +
+                '</div>' +
+            '</div>'
+
+        $('#acl-view #ul-acl').append(li)
+        $('#acl-view #body-acl').append(tab)
+
+    },
+
+    td_acl:function(view,add,edit){
+        var view_v =(view)?'checked="checked"':'';
+        var add_v =(add)?'checked="checked"':'';
+        var edit_v =(edit)?'checked="checked"':'';
+
+        var td =
+            '<td class="v-key">' +
+                '<div class="row col-12">'+
+                    '<div class="custom-control custom-checkbox custom-control-inline">'+
+                        '<input type="checkbox" class="custom-control-input view" '+view_v+'>'+
+                        '<label class="custom-control-label" for="view">View</label>'+
+                    '</div>'+
+                    '<div class="custom-control custom-checkbox custom-control-inline">'+
+                        '<input type="checkbox" class="custom-control-input add" '+add_v+'>'+
+                        '<label class="custom-control-label" for="add">Add</label>'+
+                    '</div>'+
+                    '<div class="custom-control custom-checkbox custom-control-inline">'+
+                        '<input type="checkbox" class="custom-control-input edit" '+edit_v+'>'+
+                        '<label class="custom-control-label" for="edit">Edit</label>'+
+                    '</div>'+
+                '</div>' +
+            '</td>';
+
+        return td;
     }
 
 
